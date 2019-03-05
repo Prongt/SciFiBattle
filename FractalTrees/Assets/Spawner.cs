a57d766e8473547b524b689a0b2276b626100bda
Eoin Ahern <eahern123@gmail.com>
2019-03-05 17:04:24 +0000

    Spawning works but components are not added

--- FractalTrees/Assets/Spawner.cs
+++ FractalTrees/Assets/Spawner.cs
@@ -13,12 +13,33 @@ using Random = UnityEngine.Random;
 
 public class Spawner : JobComponentSystem
 {
-    public static EndSimulationEntityCommandBufferSystem entityCommandBuffer;
+    public EndSimulationEntityCommandBufferSystem entityCommandBuffer;
     protected override void OnCreateManager()
     {
         entityCommandBuffer = World.Active.GetOrCreateManager<EndSimulationEntityCommandBufferSystem>();
 
         //entityCommandBuffer.Instantiate(SetData.enemyData.prefab);
+    }
+    
+    private struct SpawnJob : IJobProcessComponentDataWithEntity<EnemySpawnData, LocalToWorld>
+    {
+        public EntityCommandBuffer CommandBuffer;
+
+        public void Execute(Entity entity, int index, ref EnemySpawnData spawnData, ref LocalToWorld location)
+        {
+            for (int x = 0; x < spawnData.spawnCount; x++)
+            {
+                
+                    var instance = CommandBuffer.Instantiate(spawnData.prefab);
+                    var position = math.transform(location.Value, new float3(10* x, 10 * x, 10 * x));
+
+                    CommandBuffer.SetComponent(instance, new Translation {Value = position});
+                    //CommandBuffer.SetComponent(instance, );
+
+                
+            }
+            CommandBuffer.DestroyEntity(entity);
+        }
     }
 
     protected override JobHandle OnUpdate(JobHandle inputDeps)
@@ -31,24 +52,5 @@ public class Spawner : JobComponentSystem
         entityCommandBuffer.AddJobHandleForProducer(job);
 
         return job;
-    }
-    private struct SpawnJob : IJobProcessComponentDataWithEntity<EnemySpawnData, LocalToWorld>
-    {
-        public EntityCommandBuffer CommandBuffer;
-
-        public void Execute(Entity entity, int index, ref EnemySpawnData spawnData, ref LocalToWorld location)
-        {
-            for (int x = 0; x < spawnData.spawnCount; x++)
-            {
-                    var instance = CommandBuffer.Instantiate(spawnData.prefab);
-
-                    var position = math.transform(location.Value,
-                        new float3(Random.Range(-100.0f, 100.0f),
-                            Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f)));
-                    CommandBuffer.SetComponent(instance, new Translation { Value = position });
-                    //CommandBuffer.AddComponent(instance, EnemyData);
-            }
-            CommandBuffer.DestroyEntity(entity);
-        }
     }
 }
---
+++ FractalTrees/Assets/SpawnerBootstrap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ComponentData;
+using Unity.Entities;
+using UnityEngine;
+
+public class SpawnerBootstrap : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+{
+
+    public GameObject prefab;
+    public int spawnCount;
+    [SerializeField] public EnemyData enemyData;
+
+    public void DeclareReferencedPrefabs(List<GameObject> gameObjects)
+    {
+        gameObjects.Add(prefab);
+    }
+
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var spawnerData = new EnemySpawnData
+        {
+            prefab = conversionSystem.GetPrimaryEntity(prefab),
+            spawnCount = spawnCount
+        };
+        dstManager.AddComponentData(entity, spawnerData);
+        
+        dstManager.AddComponentData(entity, enemyData);
+        dstManager.SetComponentData(entity, enemyData);
+
+    }
+}
