76b338743bd904b153ec8b85d558db54ce852ec6
Eoin Ahern <1413840+Prongt@users.noreply.github.com>
2019-06-21 22:48:59 +0100

    updated packages

---
+++ ECS_SpaceBattle/Assets/Scripts/Physics/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+using Unity.Transforms;
+
+public struct PlayerData : IComponentData
+{
+    public Translation Position;
+}
